78602b92da78f076db0c826c6fbec15d82afe7f2
Raydel <raydelojeda@hotmail.com>
2023-01-07 02:48:54 +0000

    upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.header')
+@section('title')
+    home
+@endsection
+@section('content')
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>Home Page</h3>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/signup.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.header')
+@section('title')
+    Signup
+@endsection
+@section('content')
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>Signup Page</h3>
+            </div>
+        </div>
+    </div>
+@endsection
