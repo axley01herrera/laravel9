c8d7cce2fd7548eb3c838837aebb0ef4d59ee067
Raydel <raydelojeda@hotmail.com>
2023-01-07 02:24:52 +0000

    upload

---
+++ storage/framework/views/5d41875e9fca11fc6d0bf336f12f57ff14677abd.php
@@ -0,0 +1,14 @@
+
+<?php $__env->startSection('title'); ?>
+    Signup
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>Signup Page</h3>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\laravel9\resources\views/signup.blade.php ENDPATH**/ ?>
